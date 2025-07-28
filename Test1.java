2d76da085cfd04b661cd2e24c576c988d32efcf4
alexanderwong <snowhk10@gmail.com>
2025-07-28 13:31:58 +0800

    update

---
+++ Test1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+  public static void main(String[] args) {
+    List<String> stockList = new ArrayList<>();
+             stockList.add("1299.HK");
+             stockList.add("1788.HK");
+             stockList.add("9988.HK");
+             stockList.add("2828.HK");
+             stockList.add("2800.HK");
+             stockList.add("3033.HK");
+             stockList.add("1810.HK");
+             stockList.add("3690.HK");
+             stockList.add("0700.HK");
+             stockList.add("0981.HK");
+             stockList.add("9868.HK");
+             stockList.add("0939.HK");
+             stockList.add("9926.HK");             
+             stockList.add("2318.HK");
+             stockList.add("1211.HK");
+             stockList.add("1024.HK");
+             stockList.add("9992.HK");
+             stockList.add("0241.HK");
+             stockList.add("0388.HK");
+             stockList.add("3750.HK");
+             
+      System.out.println(stockList);
+      
+  }
+}
